8fbdacba094eb36edda2d213f17b198c57fdd9d9
midhunbalan <midhun.balan@anywhere.co>
2024-04-24 15:52:33 +0530

    inheritance example

--- src/com/oops/inheritace/Example.java
+++ src/com/oops/inheritace/Example.java
@@ -1,4 +1,24 @@
 package com.oops.inheritace;
 
-public class Example {
+class A{
+    public void sayHello(){
+        System.out.println("Hello from class A");
+    }
+    public void sayHi(){
+        System.out.println("Hi from class A");
+    }
+}
+
+class B extends A{
+    public void sayHello(){
+        System.out.println("Hello from class B");
+    }
+}
+
+public class Example extends B {
+    public static void main(String[] args) {
+        Example example = new Example();
+        example.sayHello();
+        example.sayHi();
+    }
 }
